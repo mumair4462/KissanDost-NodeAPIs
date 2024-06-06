6908c96e2287cdf1a0a4acde21db2f1b65664624
Daha Qalbi <qalbichamp5@gmail.com>
2024-06-06 17:52:36 +0500

    updated

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ app.use(express.json({limit: '50mb'}));
 app.use(cors());
 // to set router
 app.use('/api/auth',auth_routes)
-app.use('/api/product',product_routes)
+app.use('/api/product',product_routes);
+
+app.get('/getImg/:id', async(req, res)=>{
+    const {id} = req.params;
+
+    res.sendFile(__dirname + `/uploads/${id}`);
+})
 
 
 const start = async ()=>{
